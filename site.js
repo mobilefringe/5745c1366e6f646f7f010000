d6ee92766cf29f824e67b359342496a232d9d936
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-07-11 13:48:52 -0400

    Green Valley 1.0  /site.js

--- site.js
+++ site.js
@@ -2,6 +2,6 @@ var site_json = {
                     "name" : "The District at Green Valley Ranch",
                     "default_image" : "http://assets.codecloudapp.com/sites/5745c1366e6f646f7f010000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
                     "time_zone" : "T08:00:00Z",
-                     "instagram" : "196558634"
+                     "social_feed" : "http://greenvalley.mallmaverick.com/api/v2/greenvalley/social.json"
                     
                 }
