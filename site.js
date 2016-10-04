0ca01070561bc9307a68087b4420add6f48e375a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 12:41:38 -0400

    Green Valley 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "name" : "The District at Green Valley Ranch",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f3d9016e6f640541020000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
+                    "time_zone" : "T08:00:00Z",
+                     "social_feed" : "//greenvalley.mallmaverick.com/api/v2/greenvalley/social.json"
+                    
+                }
